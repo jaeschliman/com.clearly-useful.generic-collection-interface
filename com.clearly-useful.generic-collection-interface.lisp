7e96a2500cd90e27e0197b4ad8d742964c12e68c
Jason Aeschliman <j.aeschliman@gmail.com>
2012-09-28 12:47:35 -0700

    saner project structure (docs)

--- com.clearly-useful.generic-collection-interface.lisp
+++ com.clearly-useful.generic-collection-interface.lisp
@@ -2,126 +2,103 @@
 
 (in-package #:com.clearly-useful.generic-collection-interface)
 
-(define-interface-package com.clearly-useful.collection-protocol
-    "The collection protocol"
-  collection
-  collection-p
-  empty
-  empty-p
+(define-interface-package #:com.clearly-useful.collection-protocol
+    "Interface to the collection protocol"
+  #:collection
+  #:collection-p
+  #:empty
+  #:empty-p)
+
+(define-interface-package #:com.clearly-useful.sequence-protocol
+    "Interface to the sequence protocol"
+  #:collection
+  #:collection-p
+  #:empty
+  #:empty-p
   
-  )
-
-(define-interface-package com.clearly-useful.sequence-protocol
-    ""
-  collection
-  collection-p
-  empty
-  empty-p
-
-  seqable
-  seqable-p
-  seq
-  seq-p
-  head
-  tail
+  #:seqable
+  #:seqable-p
+  #:seq
+  #:seq-p
+  #:head
+  #:tail
   
-  doseq
-  seq-to-list
+  #:doseq
+  #:seq-to-list)
+
+(define-interface-package #:com.clearly-useful.countable-protocol
+    "Interface to the countable protocol"
+  #:collection
+  #:collection-p
+  #:empty
+  #:empty-p
   
-  )
-(define-interface-package com.clearly-useful.countable-protocol
-    ""
-  collection
-  collection-p
-  empty
-  empty-p
+  #:countable
+  #:countable-p
+  #:counted-p
+  #:count-elements)
+
+(define-interface-package #:com.clearly-useful.indexable-protocol
+    "Interface to the indexable protocol"
+  #:collection
+  #:collection-p
+  #:empty
+  #:empty-p
   
-  countable
-  countable-p
-  counted-p
-  count-elements
-  )
-(define-interface-package com.clearly-useful.indexable-protocol
-    ""
-  collection
-  collection-p
-  empty
-  empty-p
-
-  countable
-  countable-p
-  counted-p
-  count-elements
-
-  indexable
-  indexable-p
-  element-at
-
-  doindexable
+  #:countable
+  #:countable-p
+  #:counted-p
+  #:count-elements
   
-  )
-(define-interface-package com.clearly-useful.associative-protocol
-    ""
-  collection
-  collection-p
-  empty
-  empty-p
-
-  associative
-  all-keys
-  all-values
-  contains-key-p
-  value-for-key
-
-  all-keys-and-values
-  getkey
+  #:indexable
+  #:indexable-p
+  #:element-at
   
-  )
-(define-interface-package com.clearly-useful.reduceable-protocol
-    ""
-  reduceable
-  reduceable-p
-  coll-reduce
-  reduced
-  fold-left
-  reducer
-  mapping
-  filtering
-  mapcatting
-  monoid
+  #:doindexable)
+
+(define-interface-package #:com.clearly-useful.associative-protocol
+    "Interface to the associative protocol"
+  #:collection
+  #:collection-p
+  #:empty
+  #:empty-p
   
-  )
-(define-interface-package com.clearly-useful.foldable-protocol
-    ""
-  reduceable
-  reduceable-p
-  coll-reduce
-  reduced
-  fold-left
-  reducer
-  mapping
-  filtering
-  mapcatting
-  monoid
-  folder
-  foldable-p
-  foldable
-  coll-fold
-  fold
-  )
-
-#|(define-interface-package com.clearly-useful.
-
-    ""
-  collection
-  collection-p
-  empty
-  empty-p
+  #:associative
+  #:all-keys
+  #:all-values
+  #:contains-key-p
+  #:value-for-key
   
-  )
-|#
+  #:all-keys-and-values
+  #:getkey)
 
+(define-interface-package #:com.clearly-useful.reduceable-protocol
+    "Interface to the reduceable protocol"
+  #:reduceable
+  #:reduceable-p
+  #:coll-reduce
+  #:reduced
+  #:fold-left
+  #:reducer
+  #:mapping
+  #:filtering
+  #:mapcatting
+  #:monoid)
 
-
-
-
+(define-interface-package #:com.clearly-useful.foldable-protocol
+    "Interface to the foldable protocol"
+  #:reduceable
+  #:reduceable-p
+  #:coll-reduce
+  #:reduced
+  #:fold-left
+  #:reducer
+  #:mapping
+  #:filtering
+  #:mapcatting
+  #:monoid
+  #:folder
+  #:foldable-p
+  #:foldable
+  #:coll-fold
+  #:fold)
